e8bcf06f390638a13fe909530b4a43c98fc50370
fluidbyte <kent@fluidbyte.net>
2013-07-25 15:17:02 +0000

    Linting tabs, Issue #29

--- src/js/nodedit.tabs.js
+++ src/js/nodedit.tabs.js
@@ -4,7 +4,7 @@
  */
 nodedit.tabs = {
     
-    el: '#tabs',
+    el: "#tabs",
     
     overflow_timeout: null,
     
@@ -19,7 +19,7 @@ nodedit.tabs = {
             name = nodedit.filemanager.getFileName(path);
 
         // Compile template
-        nodedit.template('tab.tpl', {id: id, path: path, name: name}, function(tmpl) {
+        nodedit.template("tab.tpl", {id: id, path: path, name: name}, function(tmpl) {
             // Add tab
             nodedit.$el.find(_this.el).append(tmpl); 
             _this.setActive(id);
@@ -37,10 +37,10 @@ nodedit.tabs = {
     sortable: function () {
         var _this = this;
         nodedit.$el.find(_this.el).sortable({ 
-            axis: 'x', 
-            items: 'li',
-            containment: 'parent',
-            placeholder: 'tab-sort-placeholder',
+            axis: "x", 
+            items: "li",
+            containment: "parent",
+            placeholder: "tab-sort-placeholder",
             distance: 5
         });  
     },
@@ -51,7 +51,7 @@ nodedit.tabs = {
      */
     overflow: function () {
         var _this = this,
-            tab_els = nodedit.$el.find(_this.el).children('li'),
+            tab_els = nodedit.$el.find(_this.el).children("li"),
             w_available = nodedit.$el.find(_this.el).outerWidth(),
             tab_w, tab_count, cur_tab, new_tab, remainder;
         
@@ -60,47 +60,53 @@ nodedit.tabs = {
         // Get tab count
         tab_count = tab_els.length;
         
+        var bindNewTab = function (new_tab) {
+            
+            // Bind click on tab
+            new_tab.on("click", function () {
+                nodedit.editor.gotoInstance($(this).data("id"));
+            });
+            
+            // Bind click on close
+            new_tab.on("click","a", function () {
+                nodedit.editor.close($(this).parent("li").data("id")); 
+            });
+            
+        };
+        
         // Out of space?
         if ((tab_w*tab_count) > w_available-30) {
             // Clear any existing contents
-            nodedit.$el.find('#tabs-reveal-menu').remove();
+            nodedit.$el.find("#tabs-reveal-menu").remove();
             // Find remainder
             remainder = Math.ceil(((tab_w*tab_count)-(w_available-30))/tab_w);
             // Create reveal icon
-            nodedit.$el.find(_this.el).append('<a id="tabs-reveal" class="icon-double-angle-right"></a>');
+            nodedit.$el.find(_this.el).append("<a id=\"tabs-reveal\" class=\"icon-double-angle-right\"></a>");
             // Create reveal menu
-            nodedit.$el.append('<ul id="tabs-reveal-menu"></ul>');
+            nodedit.$el.append("<ul id=\"tabs-reveal-menu\"></ul>");
             
             // Loop in remainder tabs
             for (var i=tab_count-remainder, z=tab_count; i<z; i++){
                 // Get current tab
-                cur_tab = nodedit.$el.find(_this.el).children('li:eq('+i+')');
+                cur_tab = nodedit.$el.find(_this.el).children("li:eq("+i+")");
                 
                 // Add element to menu
-                nodedit.$el.find('#tabs-reveal-menu').append('<li data-id="'+cur_tab.data('id')+'">'+cur_tab.html()+'</li>');
+                nodedit.$el.find("#tabs-reveal-menu").append("<li data-id=\""+cur_tab.data("id")+"\">"+cur_tab.html()+"</li>");
                 
                 // Get new tab
-                new_tab = nodedit.$el.find('#tabs-reveal-menu').children('li[data-id="'+cur_tab.data('id')+'"]');
+                new_tab = nodedit.$el.find("#tabs-reveal-menu").children("li[data-id=\""+cur_tab.data("id")+"\"]");
                 
-                // Bind click on tab
-                new_tab.on('click', function () {
-                    nodedit.editor.gotoInstance($(this).data('id'));
-                });
-                
-                // Bind click on close
-                new_tab.on('click','a', function () {
-                   nodedit.editor.close($(this).parent('li').data('id')); 
-                });
+                bindNewTab(new_tab);
                 
             }
             
             // Bind click to show menu
-            nodedit.$el.find(_this.el).children('#tabs-reveal').on('click', function () { 
-                nodedit.$el.find('#tabs-reveal-menu').show();    
+            nodedit.$el.find(_this.el).children("#tabs-reveal").on("click", function () { 
+                nodedit.$el.find("#tabs-reveal-menu").show();    
             });
             
             // Hide menu on mouseleave
-            nodedit.$el.find('#tabs-reveal-menu').on('mouseleave', function() {
+            nodedit.$el.find("#tabs-reveal-menu").on("mouseleave", function() {
                 $(this).hide();
             });
             
@@ -108,8 +114,8 @@ nodedit.tabs = {
             _this.setActive(_this.getActive());
             
         } else {
-            nodedit.$el.find(_this.el).children('#tabs-reveal').remove();
-            nodedit.$el.find('#tabs-reveal-menu').remove();
+            nodedit.$el.find(_this.el).children("#tabs-reveal").remove();
+            nodedit.$el.find("#tabs-reveal-menu").remove();
         }
         
         // Bind to resize, use timeouts to prevent rapid-fire during resize
@@ -130,24 +136,24 @@ nodedit.tabs = {
      */
     close: function (id) {
         var _this = this,
-            tab = nodedit.$el.find(_this.el).children('li').filterByData('id', id),
+            tab = nodedit.$el.find(_this.el).children("li").filterByData("id", id),
             index = tab.index(),
-            maxIndex = (nodedit.$el.find(_this.el).children('li').size())-1,
+            maxIndex = (nodedit.$el.find(_this.el).children("li").size())-1,
             openIndexOf;
             
         tab.remove();
         // Find and switch to neighboring tab (if this is the active tab)
-        if (tab.hasClass('active')) {
+        if (tab.hasClass("active")) {
             if (index>0) {
-                // Prefer move to left, check that this isn't first tab
+                // Prefer move to left, check that this isn"t first tab
                 openIndexOf = index-1;
             } else if (index<maxIndex) {
-                // If not the last item, open the item to right (will match the closed item's index)
+                // If not the last item, open the item to right (will match the closed item"s index)
                 openIndexOf = index;
             }
             
             if (openIndexOf!==undefined) {
-                var newId = nodedit.$el.find(_this.el).children('li:eq('+openIndexOf+')').attr('data-id');
+                var newId = nodedit.$el.find(_this.el).children("li:eq("+openIndexOf+")").attr("data-id");
                 nodedit.editor.gotoInstance(newId);
             }
         }
@@ -166,15 +172,15 @@ nodedit.tabs = {
      */
     rename: function (oldPath, newPath, id) {
         var _this = this,
-            tab = nodedit.$el.find(_this.el).children('li').filterByData('id', id),
-            curPath = tab.attr('title');
+            tab = nodedit.$el.find(_this.el).children("li").filterByData("id", id),
+            curPath = tab.attr("title");
         
         // Change title attr
-        tab.attr('title', tab.attr('title').replace(oldPath, newPath));
+        tab.attr("title", tab.attr("title").replace(oldPath, newPath));
         
         if (curPath===oldPath) {
             // Full path match, change label
-            tab.children('label').text(nodedit.filemanager.getFileName(newPath));
+            tab.children("label").text(nodedit.filemanager.getFileName(newPath));
         }
         
     },
@@ -186,10 +192,10 @@ nodedit.tabs = {
      */
     setActive: function (id) {
         var _this = this;
-        nodedit.$el.find(_this.el).children('li').removeClass('active');
-        nodedit.$el.find('#tabs-reveal-menu').children('li').removeClass('active');
-        nodedit.$el.find(_this.el).children('li').filterByData('id', id).addClass('active');
-        nodedit.$el.find('#tabs-reveal-menu').children('li').filterByData('id', id).addClass('active');
+        nodedit.$el.find(_this.el).children("li").removeClass("active");
+        nodedit.$el.find("#tabs-reveal-menu").children("li").removeClass("active");
+        nodedit.$el.find(_this.el).children("li").filterByData("id", id).addClass("active");
+        nodedit.$el.find("#tabs-reveal-menu").children("li").filterByData("id", id).addClass("active");
     },
     
     /**
@@ -198,8 +204,8 @@ nodedit.tabs = {
      */
     getActive: function () {
         var _this = this;
-        if (nodedit.$el.find(_this.el).children('li.active').length!==0) {
-            return nodedit.$el.find(_this.el).children('li.active').data('id');
+        if (nodedit.$el.find(_this.el).children("li.active").length!==0) {
+            return nodedit.$el.find(_this.el).children("li.active").data("id");
         } else {
             return false;
         }
@@ -212,7 +218,7 @@ nodedit.tabs = {
      */
     bindClose: function (id) {
         var _this = this;
-        nodedit.$el.find(_this.el).find('li').filterByData('id', id).on('click', 'a', function (e) {
+        nodedit.$el.find(_this.el).find("li").filterByData("id", id).on("click", "a", function (e) {
             e.stopPropagation(); // Stop propagation to tab (li) item
             nodedit.editor.close(id);
         });
@@ -225,7 +231,7 @@ nodedit.tabs = {
      */
     bindClick: function (id) {
         var _this = this;
-        nodedit.$el.find(_this.el).find('li').filterByData('id', id).on('click', function () {
+        nodedit.$el.find(_this.el).find("li").filterByData("id", id).on("click", function () {
             nodedit.editor.gotoInstance(id);
         });
     },
@@ -237,11 +243,11 @@ nodedit.tabs = {
      */
     markChanged: function (id) {
         var _this = this,
-            label = nodedit.$el.find(_this.el).children('li').filterByData('id', id).children('label');
+            label = nodedit.$el.find(_this.el).children("li").filterByData("id", id).children("label");
         
         // Compare to initial state
-        if (nodedit.editor.getContent(id)!=nodedit.editor.instances[id].content) {
-            label.addClass('changed');
+        if (nodedit.editor.getContent(id)!==nodedit.editor.instances[id].content) {
+            label.addClass("changed");
         } else {
             _this.markUnchanged(id);
         }
@@ -254,7 +260,7 @@ nodedit.tabs = {
      */
     markUnchanged: function (id) {
         var _this = this;
-        nodedit.$el.find(_this.el).children('li').filterByData('id', id).children('label').removeClass('changed');
+        nodedit.$el.find(_this.el).children("li").filterByData("id", id).children("label").removeClass("changed");
     },
     
     /**
@@ -267,12 +273,12 @@ nodedit.tabs = {
             i;
         if (id) {
             // Check for specific editor with unsaved changes
-            if (nodedit.$el.find(_this.el).children('li').filterByData('id', id).children('label').hasClass('changed')) {
+            if (nodedit.$el.find(_this.el).children("li").filterByData("id", id).children("label").hasClass("changed")) {
                 return true;
             }
         } else {
             for (i in nodedit.editor.instances) {
-                if (nodedit.$el.find(_this.el).children('li').filterByData('id', id).children('label').hasClass('changed')) {
+                if (nodedit.$el.find(_this.el).children("li").filterByData("id", id).children("label").hasClass("changed")) {
                     return true;
                 }
             }
